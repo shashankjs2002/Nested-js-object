aa77bf8b1d5bde2dc214dac418bc97b7afedee39
shashankjs2002 <93313748+shashankjs2002@users.noreply.github.com>
2022-06-17 20:07:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,4 @@
 
-// const url = items;
-// console.log(url)
-
-// const fetchData = async (url) => {
-//     let data = await fetch(url);
-//     let parsedData = await data.json(); 
-//     console.log(parsedData);
-// }
-
-
-// fetch("./items.json")
-// .then(response => {
-//    return response.json();
-// })
-// .then( async data =>{
-//     console.log(data)
-//     let parsedData = await data.json();
-//     console.log(parsedData)
-    
-// } );
-
-// const fetchData = async () =>{
-
-
-// let data = await fetch("./items.json");
-// console.log(data)
-// let parsedData = await data.json(); 
-// console.log(parsedData);
-// console.log(parsedData.items);
-// parsedData.items.map(async (e) => {
-//    console.log(e)
-// //    e = await e.
-// //    console.log(e)
-
-// })
-
-
-
-// }
-
 
 // const arr = { Items : [
 //     "Item 1", 
@@ -156,39 +116,7 @@ const arr = {
     ]
 }
 
-// console.log(arr)
-// console.log(arr.Items)
-// console.log(Object.keys(arr))
-// let Item = Object.keys(arr)[0]
-// let ItemValue = Object.values(arr)
-// console.log(ItemValue)
-// console.log(Item)
-// // let i = Object.values(Item)
-// let i = ItemValue
-// console.log(i)
-// (i.forEach(element => {
-//     console.log(element.value)
-// }))
 
-
-// let printData = async (arr) => {
-//     i = arr.Items
-//     (i.forEach(element => {
-//         console.log(element)
-//         items.map( async (e) => {
-//             if(typeof(e) === 'object'){
-//                 console.log(Object.keys(arr))
-//                 printData(e)
-//             }
-//         })
-//     }))    
-// }
-
-function clicked(element){
-    
-    // completelist.innerHTML += `<ol id='${Object.keys(element)[0]}'>`+ "</ol>"
-    printData(element)
-}
 
 
 let printData = async (arr) => {
@@ -241,47 +169,4 @@ function show (id) {
     console.log(id.style.color)
 
 }
-
-//     (i.forEach(element => {
-//         console.log(element)
-//         element.map( async (e) => {
-//             if(typeof(e) === 'object'){
-//                 console.log(Object.keys(arr))
-//                 printData(e)
-//             }
-//         })
-//     }))    
-// }
 printData(arr)
-// const fetchData = async () =>{
-
-// let data = await fetch(arr);
-// console.log(data)
-// let parsedData = await data.json(); 
-// console.log(parsedData);
-// }
-// items.map(async (e) => {
-//     console.log(e)
-//     await console.log(typeof(e))
-//     if(typeof(e) === 'object'){
-//         let data = await fetch(e);
-//         let parsedData = await data.json(); 
-//         console.log(parsedData);
-//         console.log(data)
-//         console.log("Inside e")
-//     }
-// })
-
-
-// fetchData()
-
-
-
-// var counter= 4;
-
-// function addelement() {
-// var completelist= document.getElementById("item");
-
-// completelist.innerHTML += "<li>Item " + counter + "</li>";
-// counter++;
-// }
